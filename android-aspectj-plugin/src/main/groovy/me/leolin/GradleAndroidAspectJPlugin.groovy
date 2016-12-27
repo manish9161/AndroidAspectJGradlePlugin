8229cb8c3d3d631a6a8ee79f7474be42445d333c
Vincent Brison <vincent.brison@applidium.com>
2016-12-27 10:59:57 +0100

    Refactor plugin

--- android-aspectj-plugin/src/main/groovy/me/leolin/GradleAndroidAspectJPlugin.groovy
+++ android-aspectj-plugin/src/main/groovy/me/leolin/GradleAndroidAspectJPlugin.groovy
@@ -7,7 +7,6 @@ import org.aspectj.bridge.MessageHandler
 import org.aspectj.tools.ajc.Main
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
 /**
  * @author leolin
  */
@@ -15,20 +14,16 @@ class GradleAndroidAspectJPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        def isAppProject = project.plugins.withType(AppPlugin)
-        def isLibProject = project.plugins.withType(LibraryPlugin)
-        def hasRetrolambda = project.plugins.hasPlugin('me.tatarka.retrolambda')
-
-        final def log = project.logger
-        final def variants
-        if (isAppProject) {
-            variants = project.android.applicationVariants
-        } else if (isLibProject) {
-            variants = project.android.libraryVariants
-        } else {
-            throw new IllegalStateException("Must be android project or android-library project.")
+        def variants = getVariants(project)
+        ensureAspectJrtDependencyIsSatisfied(project)
+        project.afterEvaluate {
+            variants.all { variant ->
+                doWeave(variant, project)
+            }
         }
+    }
 
+    private void ensureAspectJrtDependencyIsSatisfied(Project project) {
         project.repositories {
             mavenCentral()
         }
@@ -37,60 +32,72 @@ class GradleAndroidAspectJPlugin implements Plugin<Project> {
         project.dependencies {
             compile "org.aspectj:aspectjrt:$aspectjVersion"
         }
+    }
 
-        project.afterEvaluate {
+    private boolean hasRetroLambda(Project project) {
+        project.plugins.hasPlugin('me.tatarka.retrolambda')
+    }
 
-            variants.all { variant ->
-                def buildTypeName = variant.name.capitalize()
+    private getVariants(Project project) {
+        def isAppProject = project.plugins.withType(AppPlugin)
+        def isLibProject = project.plugins.withType(LibraryPlugin)
+        if (isAppProject) {
+            return project.android.applicationVariants
+        } else if (isLibProject) {
+            return project.android.libraryVariants
+        } else {
+            throw new IllegalStateException("Must be android project or android-library project.")
+        }
+    }
 
-                def aopTask = project.task("compile${buildTypeName}AspectJ") {
-                    doLast {
-                        String[] args = [
-                                "-showWeaveInfo",
-                                "-1.5",
-                                "-inpath", javaCompile.destinationDir.toString(),
-                                "-aspectpath", javaCompile.classpath.asPath,
-                                "-d", javaCompile.destinationDir.toString(),
-                                "-classpath", javaCompile.classpath.asPath,
-                                "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
-                        ]
+    private void doWeave(variant, Project project) {
+        def buildTypeName = variant.name.capitalize()
+        def aopTask = project.task("compile${buildTypeName}AspectJ") {
+            doLast {
+                String[] args = [
+                        "-showWeaveInfo",
+                        "-1.5",
+                        "-inpath", javaCompile.destinationDir.toString(),
+                        "-aspectpath", javaCompile.classpath.asPath,
+                        "-d", javaCompile.destinationDir.toString(),
+                        "-classpath", javaCompile.classpath.asPath,
+                        "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
+                ]
 
-                        MessageHandler handler = new MessageHandler(true);
-                        log.quiet("Start aspectJ work...")
-                        log.quiet("Full ajc build args: ${Arrays.toString(args as String[])}\n\n")
-                        new Main().run(args, handler);
-                        for (IMessage message : handler.getMessages(null, true)) {
-                            switch (message.getKind()) {
-                                case IMessage.ABORT:
-                                case IMessage.ERROR:
-                                case IMessage.FAIL:
-                                    if (message.thrown != null) {
-                                        log.error(message.message, message.thrown)
-                                    } else {
-                                        log.error(message.message)
-                                    }
-                                    break;
-                                case IMessage.WARNING:
-                                    if (message.thrown != null) {
-                                        log.warn(message.message, message.thrown)
-                                    } else {
-                                        log.warn(message.message)
-                                    }
-                                    break;
-                                case IMessage.INFO:
-                                case IMessage.DEBUG:
-                                    break;
+                MessageHandler handler = new MessageHandler(true);
+                log.quiet("Start aspectJ work...")
+                log.quiet("Full ajc build args: ${Arrays.toString(args as String[])}\n\n")
+                new Main().run(args, handler);
+                for (IMessage message : handler.getMessages(null, true)) {
+                    switch (message.getKind()) {
+                        case IMessage.ABORT:
+                        case IMessage.ERROR:
+                        case IMessage.FAIL:
+                            if (message.thrown != null) {
+                                log.error(message.message, message.thrown)
+                            } else {
+                                log.error(message.message)
                             }
-                        }
+                            break;
+                        case IMessage.WARNING:
+                            if (message.thrown != null) {
+                                log.warn(message.message, message.thrown)
+                            } else {
+                                log.warn(message.message)
+                            }
+                            break;
+                        case IMessage.INFO:
+                        case IMessage.DEBUG:
+                            break;
                     }
                 }
-
-                if (hasRetrolambda) {
-                    project.tasks["compileRetrolambda$buildTypeName"].finalizedBy(aopTask)
-                } else {
-                    variant.javaCompile.finalizedBy(aopTask)
-                }
             }
+        }
+
+        if (hasRetroLambda(project)) {
+            project.tasks["compileRetrolambda$buildTypeName"].finalizedBy(aopTask)
+        } else {
+            variant.javaCompile.finalizedBy(aopTask)
         }
     }
 }
